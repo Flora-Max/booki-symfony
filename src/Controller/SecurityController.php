389de5169d7e2b253c31a50a2f2eec8834efd672
Flora Boularan <flora.b@gmail.com>
2022-05-02 11:36:42 +0200

    WIP: register method

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,20 +23,13 @@ use Symfony\Component\Serializer\Serializer;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="app_login", methods="post")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->json([
+            'bonjour' => 'bonjour'
+        ]);
     }
 
     /**
